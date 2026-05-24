34922ff9ec9093a99f069fe98dd95a08c6adf12a
kkrithiga3-a11y <kkrithiga3@gmail.com>
2026-05-24 09:14:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const checkBoxes = document.querySelectorAll(".check-box");
 
+checkBoxes.forEach(box => {
+    box.addEventListener("click", () => {
+        box.classList.toggle("active");
+    });
+});
